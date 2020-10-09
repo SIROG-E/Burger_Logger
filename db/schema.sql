0860c7ecc77857e93c96ceea8636f9f626b92842
Evelyn Goris <goris.evelyn@gmail.com>
2020-10-08 23:22:24 -0500

    copy/pasted to server.js, implemented main.handlebars, changed table name in schema.sql and seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers_table(
+CREATE TABLE burgers(
 	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
+	burger_name varchar(50) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
     burger_time timestamp,
 	PRIMARY KEY (id)
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers_table (burger_name) VALUES ('Cheese Burger');
-INSERT INTO burgers_table (burger_name) VALUES ('Hamburger');
-INSERT INTO burgers_table (burger_name) VALUES ('Bacon Cheese Burger');
-INSERT INTO burgers_table (burger_name) VALUES ('Bacon Cheese Burger');
+USE burgers_db;
+
+INSERT INTO burgers(burger_name) 
+VALUES ('Cheese Burger', false), ('Hamburger', false), ('Bacon Cheese Burger', false), ('Bacon Cheese Burger');
