b46b991598d9d770a054f51113cc82a53e68c826
Evelyn Goris <goris.evelyn@gmail.com>
2020-10-08 22:35:14 -0500

    implemented seeds.sql to instert to burgers_table. Fixed syntax in squema.sql

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers_table (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers_table (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers_table (burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers_table (burger_name) VALUES ('Bacon Cheese Burger');
